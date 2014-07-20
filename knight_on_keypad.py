ed563eca05a8deaeaa9d71760baadb67065f19b3
harshitbahl <harshit.bahl@me.com>
2014-07-19 23:49:24 -0500

    Final Tree is ready

--- knight_on_keypad.py
+++ knight_on_keypad.py
@@ -31,111 +31,60 @@ class Node(object):
         for c in self:
             yield [i for i in c.depth_first()]
 
-def generate_node_pool():
-    pool = {}
-    for i in [1,2,3,4,6,7,8,9]:
-        pool[i]=Node(i)
-    return pool
-    
-
 class parentNode(object):
     
     def __init__(self, parentNode):
         self._parentNodes = parentNode
     
     def __repr__(self):
-        return self._parentNodes
+        return str(self._parentNodes)
     
     def update_parent_nodes(self,parentNodes):
-        self._parentNodes = [i for i in parentNodes]
+        for new_parents in parentNodes:
+            self._parentNodes.append(new_parents)
     
-    
-
-def sentinal(parent_node):
-    return len([i for i in parent_node.depth_first()])
-
-def genrate_tree(node_root,parent_node):
-#    print sentinal(parent_node)
-    parent_node = node_root
-    if sentinal(parent_node)<=3:
-        [node_root.add_child(Node(i)) for i in jump[node_root._value]]
-        print node_root._children[0],node_root._children[1]
-        genrate_tree(node_root._children[0],parent_node)
-        genrate_tree(node_root._children[1],parent_node)
-    else:
-        return
-
+    def refresh(self):
+        self._parentNodes  = []
         
+    
+    
 
 def get_root():
     return None
 
 def get_child_nodes(root):
-    return [Node(i) for i in jump.get(root)]
+    return [Node(i) for i in jump.get(root._value)]
     
 def update_children(root):
     childrens = get_child_nodes(root)
     [root.add_child(child) for child in childrens]
-    
-        
-        
-    
 
-def main():
-    root =  Node(1)
-    parentNode([root])
-    for i in range(9):    
-        for node in parentNode._parentNodes:
-            update_children(node)
-            parentNode.update_parent_nodes(node._children)
-    pprint.pprint(root.depth_first)
-    
-    
-    
-    
+from collections import Iterable
 
-        
-    
-         
-    
-#    #1    
-#    for j in tree_top._children:
-#        [j.add_child(Node(i)) for i in jump[j._value]]
-#    #2
-#    for k in j._children:
-#        [k.add_child(Node(i)) for i in jump[k._value]]
-#    # 3
-#    for l in k._children:
-#        [l.add_child(Node(i)) for i in jump[l._value]]
-#    #4
-#    for m in l._children:
-#        [m.add_child(Node(i)) for i in jump[m._value]]
-#    #5
-#    for n in m._children:
-#        [n.add_child(Node(i)) for i in jump[n._value]]
-#    return node_root
+def flatten(items, ignore_types=(str, bytes)):
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            yield [ i for i in flatten(x, ignore_types)]
+        else:
+            yield x
+
     
 # Example
 if __name__ == '__main__':
-    main()
-#    generate_node_pool()
-#    node_root = Node(1)
-#    sentinal_prime = sentinal(node_root)
-#    tt = genrate_tree(node_root,node_root)
-#    print tt.depth_first()
-#    for ch in tt.depth_first():
-#        print ch
-    
-#    root = Node(1)
-#    child1 = Node(jump[root._value][0])
-#    child2 = Node(jump[root._value][1])
-#    root.add_child(child1)
-#    root.add_child(child2)
-#    child1.add_child(Node(jump[child1._value][0]))
-#    child1.add_child(Node(jump[child1._value][1]))
-#    child2.add_child(Node(jump[child2._value][0]))
-#    child2.add_child(Node(jump[child2._value][1]))
-#    print root,root._children
-#    for ch in root.depth_first():
-#        print ch
+    root =  Node(1)
+    parent_nodes = parentNode([root])
+    for i in range(10):
+        #lets clean the parentnode and create copy
+        parent_node_copy = parent_nodes._parentNodes
+        # Reset the parenode Class
+        parent_nodes.refresh()
+        for node in parent_node_copy:
+#            print 'Parent Node Copy', parent_node_copy
+            update_children(node)
+            parent_nodes.update_parent_nodes(node._children)
+    pprint.pprint([ch for ch in root.depth_first()])
+
+
+
+
 
